c61725277be1b7b4ca355b47c8c6b0d0980a5a66
Petya <22119@uktc-bg.com>
2026-02-23 22:55:55 +0200

    Refactor header structure and remove theme switch

--- header.php
+++ header.php
@@ -1,15 +1,11 @@
 <header class="custom-header">
-  <div class="container nav-flex">
+  <div class="nav-flex">
     <a href="index.php" class="logo">Гатанки</a>
     <ul class="nav">
       <li><a class="nav-link" href="index.php">Home</a></li>
       <li><a class="nav-link" href="about.php">About</a></li>
       <li><a class="nav-link" href="login.php">Log In</a></li>
       <li><a class="nav-link" href="signup.php">Sign Up</a></li>
-      <div class="theme-switch">
-        <input type="checkbox" id="theme-toggle">
-        <label for="theme-toggle">🌙</label>
-      </div>
     </ul>
   </div>
 </header>
