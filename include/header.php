9bab010e361d8f7c3b315d87d694c43586ea7fa2
Petya <22119@uktc-bg.com>
2026-03-11 08:28:38 +0200

    Add files via upload

---
+++ include/header.php
@@ -0,0 +1,11 @@
+<header class="custom-header">
+  <div class="nav-flex">
+    <a href="index.php" class="logo">Гатанки</a>
+    <ul class="nav">
+      <li><a class="nav-link" href="index.php">Home</a></li>
+      <li><a class="nav-link" href="about.php">About</a></li>
+      <li><a class="nav-link" href="login.php">Log In</a></li>
+      <li><a class="nav-link" href="signup.php">Sign Up</a></li>
+    </ul>
+  </div>
+</header>
